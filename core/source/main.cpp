a6ed5fd02b331f8d1498b984e60c4fe6937fce18
= <22070@ma-web.nl>
2019-03-25 11:14:07 +0100

    added visual studio build support

--- core/source/main.cpp
+++ core/source/main.cpp
@@ -30,26 +30,27 @@ int main(int argc, char const *argv[])
   Object * object = new Object({});
   object->addComponent(new Transform(Vec3<float>(0, -1, 0), Vec3<float>(1, 1, 1), Vec3<float>(0, 0, 0), "sponza", {"None"}, object));
 
-  Object * bunny = new Object({});
-  bunny->addComponent(new Transform(Vec3<float>(0, 1.8, 0), Vec3<float>(1, 1, 1), Vec3<float>(0, 0, 0), "bunny", {}, bunny));
-  bunny->addComponent(new CollisionComponent(false, new AABB(Vec3<float>(0, 1.8, 0), Vec3<float>(0.5, 0.5, 0.5)), 0, bunny));
-  bunny->getComponent<CollisionComponent>()->getCollider()->syncPos(&bunny->getComponent<Transform>()->getPos());
+  // Object * bunny = new Object({});
+  // bunny->addComponent(new Transform(Vec3<float>(0, 1.8, 0), Vec3<float>(1, 1, 1), Vec3<float>(0, 0, 0), "bunny", {}, bunny));
+  // bunny->addComponent(new CollisionComponent(false, new AABB(Vec3<float>(0, 1.8, 0), Vec3<float>(0.5, 0.5, 0.5)), 0, bunny));
+  // bunny->getComponent<CollisionComponent>()->getCollider()->syncPos(&bunny->getComponent<Transform>()->getPos());
 
-  // Object * instanced = new Object({});
-  // InstancedTransform * it = new InstancedTransform(instanced);
-  // for (int x = -10; x < 10; x++) {
-  //   for (int y = 2; y < 10; y++) {
-  //     for (int z = -10; z < 10; z++) {
-  //       Object * o = new Object({});
-  //       o->addComponent(new RotateTransform(Vec3<float>(x, 1.8 + y, z), Vec3<float>(0.2, 0.2, 0.2), Vec3<float>(0, 90, 0), "bunny", {"initialShadingGroup"}, engine.getInput(), o));
-  //       objects.push_back(o);
-  //       it->addToInstance(o->getComponent<Transform>());
-  //     }
-  //   }
-  // }
-  // instanced->addComponent(it);
+  Object * instanced = new Object({});
+  InstancedTransform * it = new InstancedTransform(instanced);
+  for (int x = -10; x < 10; x++) {
+    for (int y = 2; y < 10; y++) {
+      for (int z = -10; z < 10; z++) {
+        Object * o = new Object({});
+        o->addComponent(new RotateTransform(Vec3<float>(x, 1.8 + y, z), Vec3<float>(0.2, 0.2, 0.2), Vec3<float>(0, 90, 0), "bunny", {"initialShadingGroup"}, engine.getInput(), o));
+        objects.push_back(o);
+        it->addToInstance(o->getComponent<Transform>());
+      }
+    }
+  }
+  instanced->addComponent(it);
 
-  objects.push_back(bunny);
+  // objects.push_back(bunny);
+  // objects.push_back(instanced);
   objects.push_back(object);
   objects.push_back(camera);
   CollisionModule * module = new CollisionModule(50, 4);
